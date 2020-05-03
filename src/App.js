0364ea1b81cfb4786d1ba53c8c851f076ebc3f51
elbader17 <badereduardo@gmail.com>
2020-05-03 18:28:43 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -5,19 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav className="navbar navbar-dark bg-dark"><p className="text-white">probando</p></nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
